36f042a2247a2458f6a2394a0c66bae9edccb099
Marshall-mk <muhammadkabirhamzah@gmail.com>
2024-11-21 16:08:07 +0100

    obfucated all code

--- src/models.py
+++ src/models.py
@@ -1,14 +1,1 @@
-import timm
-import torch.nn as nn
-
-def mobilenet_v2(output=1, ckpt=None):
-    model = timm.create_model("mobilenetv2_100", pretrained=True, checkpoint_path=ckpt)
-    num_ftrs = model.classifier.in_features
-    model.classifier = nn.Linear(num_ftrs, output)
-    return model
-
-def mobilenet_v3(output=1, ckpt=None):
-    model = timm.create_model("mobilenetv3_large_100", pretrained=True, checkpoint_path=ckpt)
-    num_ftrs = model.classifier.in_features
-    model.classifier = nn.Linear(num_ftrs, output)
-    return model
+# models.py code here
